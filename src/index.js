2ebc78fdf1e28e663b1feac03789354aca1496e9
Golam Sarwar <sarwar@linkstaff.co.jp>
2022-01-31 18:06:48 +0900

    Code refactoring

--- src/index.js
+++ src/index.js
@@ -2,20 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { lat: null, errorMessage: '' };
+    state = { lat: null, errorMessage: '' };
 
+    componentDidMount() {
+        //Lifecycle Method
+        //Get called when the component is loaded on the screen after the render method. Good place to make a data loading job.
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({ lat: position.coords.latitude })
-            },
-            err => {
-                this.setState({ errorMessage: err.message })
-            }
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
         );
+        console.log('My component was rendered to the screen');
     }
+
+    componentDidUpdate() {
+        //Lifecycle Method
+        //Whenever a state gets changed. It call render and then this method.
+        console.log('My component was just updated');
+    }
+
+    componentWillUnmount() {
+        //To cleanup stuffs
+    }
+
     render() {
+        //Lifecycle Method
+        // Render returns only JSX. No logical job, network request or other than JSX tasks.
         if (!this.state.errorMessage && !this.state.lat) {
             return <div>Loading</div>
         }
